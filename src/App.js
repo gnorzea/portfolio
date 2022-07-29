e2334d7d8c9895f77aadb1bd30086e94f51ac52c
Thunnder <gnorzea@gmail.com>
2022-07-29 06:53:39 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PageHandler from './components/PageHandler';
-
+import Footer from './components/Pages/Footer/index'
 
 function App() {
-return(
-<PageHandler></PageHandler>
-)
-  
+    return (
+        <div>
+            <main>
+                <PageHandler></PageHandler>
+            </main>
+            <Footer></Footer>
+        </div>
+
+    )
+
 }
 
 export default App;
---
+++ src/components/Pages/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer () {
+    return(
+        <section className='footer'>
+               Copyright © 2022 | GRN | All Rights Reserved
+        </section>
+    )
+}
+  
+export default Footer;

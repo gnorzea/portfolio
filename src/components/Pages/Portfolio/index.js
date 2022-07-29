f8642774e18b60d36de1f2f924d0fd16d0081141
Thunnder <gnorzea@gmail.com>
2022-07-29 04:40:25 -0400

    portfolio

--- src/components/Pages/Portfolio/index.js
+++ src/components/Pages/Portfolio/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import GudEats from '../../../assets/Images/GudEats.png';
+import RunBuddy from '../../../assets/Images/Run-Buddy.png';
+import Taskem from '../../../assets/Images/taskem.png';
 
 function Portfolio (){
 return (
   <section className='portfolio'>
-    test
+    <div>
+        <button>
+        <a href="https://miscmich.github.io/congenial-engine/?" target="_blank" rel="noopener noreferrer">GudEats</a>
+        </button>
+    </div>
+    <div className='inline'>
+        <a href="https://miscmich.github.io/congenial-engine/?" target="_blank" rel="noopener noreferrer">
+            <img src={GudEats} alt="GudEats screenshot" style={{ width: "60%" }}></img>
+        </a>
+    </div>
+    <div>
+        <button>
+        <a href="https://gnorzea.github.io/run-buddy/?" target="_blank" rel="noopener noreferrer">Run Buddy</a>
+        </button>
+    </div>
+    <div className='inline'>
+        <a href="https://gnorzea.github.io/run-buddy/" target="_blank" rel="noopener noreferrer">
+            <img src={RunBuddy} alt="RunBuddy screenshot" style={{ width: "60%" }}></img>
+        </a>
+    </div>
+    <div>
+        <button>
+        <a href="https://gnorzea.github.io/taskem/?" target="_blank" rel="noopener noreferrer">Taskem</a>
+        </button>
+    </div>
+    <div className='inline'>
+        <a href="https://gnorzea.github.io/taskem/" target="_blank" rel="noopener noreferrer">
+            <img src={Taskem} alt="Taskem screenshot" style={{ width: "60%" }}></img>
+        </a>
+    </div>
+    <div>
+        
+    </div>
 
   </section>  
 )
